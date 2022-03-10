c96f1dc709c5dba914ba2982d28e14a747d6d1c2
Tharsipa Nadarajah <90727247+Tharsipa@users.noreply.github.com>
2022-03-09 22:59:08 -0500

    Added the JavaScript file for the product pages related to issue #11
    Added required functions to the JavaScript file

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const imgs = document.querySelectorAll(".img-select a");
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+  imgItem.addEventListener("click", (event) => {
+    event.preventDefault();
+    imgId = imgItem.dataset.id;
+    slideImage();
+  });
+});
+
+function slideImage() {
+  const displayWidth = document.querySelector(
+    ".img-showcase img:first-child"
+  ).clientWidth;
+
+  document.querySelector(".img-showcase").style.transform = `translateX(${
+    -(imgId - 1) * displayWidth
+  }px)`;
+}
+window.addEventListener("resize", slideImage);
+function displayText() {
+  var text = document.getElementById("description-text");
+  if (text.style.display == "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
