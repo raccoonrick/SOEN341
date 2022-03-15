28dacf2f438ee7d915ffa1da99b88dbf2f9b6c1c
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2022-03-14 20:08:22 -0400

    Updated and fixed issues in the categories #10
    Added the new changes of the category pages.

--- categories/fitness.php
+++ categories/fitness.php
@@ -1,68 +1,80 @@
-<?php
-include "../header.php";
-// require_once "../config.php";
-$sql = "SELECT * FROM products WHERE product_category='Fitness'";
-        $result = $link->query($sql);
-?>
-<link rel =  "stylesheet" href = "../css/subcat.css">
-    <link rel =  "stylesheet" href = "../css/headernfooter.css">
-    <link rel="stylesheet" href="/css/cartStyle.css">
-     <!--ICONS-->
-   <script src="https://unpkg.com/boxicons@2.1.1/dist/boxicons.js"></script>
-   <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-<body>
-    
+   <?php include "../header.php";?>
+    <link rel =  "stylesheet" href = "../css/subcat.css">
+
 
     <div class="container-fluid" style=" text-align:center">
         <div class="row">
             <div class="col-lg-12 title">
-                <h3>Fitness</h3>
+                <h3>Fitness Equipment</h3>
             </div>
         </div>
 <br/>
 <br/><br/>
     <div class="row">
-    <?php
-        
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="../src/Bottle.html" class="image">
+                        <img class="pic-1 product-img" src="../img/bottle1.jpg">
+                        <img class="pic-2" src="../img/bottle3.jpg">
+                    </a>
+                    <a href="" class="product-like-icon"><i class="far fa-heart"></i></a>
+                    <ul class="product-links">
+                        <li><a href="#"><i class="far fa-heart"></i></a></li>
+                        <li><a href="#"><i class="fa fa-random"></i></a></li>
+                        <li><a href="#"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart add-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title product-title"><a href="#">Under Armour Water bottle</a></h3>
+                    <div class="price">$12.99</div>
+                </div>
+            </div>
+        </div>
 
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()){
-                
-                $images = explode("|",$row["product_img"]);
-                echo "<div class='col-md-3 col-sm-6'>";
-                echo "<div class='product-grid'>";
-                echo "<div class='product-image'>";
-                echo "<a href='../products/" . $row["product_link"] . "'class='image'>";
-                if(sizeof($images < 2)){
-                    echo "<img class='pic-1' src='../img/" . $images[0] . "'>";
-                    echo "<img class='pic-2' src='../img/" . $images[0] . "'>";
-                }
-                else{
-                    echo "<img class='pic-1' src='../img/" . $images[0] . "'>";
-                    echo "<img class='pic-2' src='../img/" . $images[1] . "'>";
-                }
-                echo "</a>";
-                echo "<a href='' class='product-like-icon'><i class='far fa-heart'></i></a>";
-                echo "<ul class='product-links'>";
-                echo "<li><a href='#'><i class='far fa-heart'></i></a></li>";
-                echo "<li><a href='#'><i class='far fa-random'></i></a></li>";
-                echo "<li><a href='#'><i class='far fa-eye'></i></a></li>";
-                echo "<li><a href='#'><i class='far fa-shopping-cart'></i></a></li>";
-                echo "</ul>";
-                echo "</div>";
-                echo "<div class='product-content'>";
-                echo "<h3 class='title product-title'><a href='#'>". $row["product_name"] . "</a></h3>";
-                if($row["product_onsale"] == 1){
-                    echo "<div class='price'>$" . $row["product_saleprice"] . "</div>";
-                }
-                else{
-                    echo "<div class='price'>$" . $row["product_price"] . "</div>";
-                }
-                echo "</div>";
-                echo "</div>";
-                echo "</div>";
-            }
-        }
-        ?>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="../src/Dumbbell.html" class="image">
+                        <img class="pic-1 product-img" src="../img/Dumbbell1.jpg">
+                        <img class="pic-2" src="../img/Dumbbell3.jpg">
+                    </a>
+                    <a href="" class="product-like-icon"><i class="far fa-heart"></i></a>
+                    <ul class="product-links">
+                        <li><a href="#"><i class="far fa-heart"></i></a></li>
+                        <li><a href="#"><i class="fa fa-random"></i></a></li>
+                        <li><a href="#"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart add-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title product-title"><a href="#">50 lbs dumbbell</a></h3>
+                    <div class="price">$64.99</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="../src/SportsBag.html" class="image">
+                        <img class="pic-1 product-img" src="../img/SportsBag1.jpg">
+                        <img class="pic-2" src="../img/SportsBag3.jpg">
+                    </a>
+                    <a href="" class="product-like-icon"><i class="far fa-heart"></i></a>
+                    <ul class="product-links">
+                        <li><a href="#"><i class="far fa-heart"></i></a></li>
+                        <li><a href="#"><i class="fa fa-random"></i></a></li>
+                        <li><a href="#"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart add-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title product-title"><a href="#">Nike gym bag</a></h3>
+                    <div class="price">$30.99</div>
+                </div>
+            </div>
+        </div>
     </div>
+
     <?php include "../footer.php";?>
