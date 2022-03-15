764dfe3a327eb7cda9e55c6222c2d2bf3688863e
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2022-03-14 20:24:25 -0400

    Updated the cart js #12 #13 #14 #15 #16 #19

--- js/cart.js
+++ js/cart.js
@@ -1,142 +1,127 @@
-// CART
-let cartIcon = document.querySelector('#cart-icon');
-let cart = document.querySelector('.cart');
-let closeCart = document.querySelector('#close-cart');
+var promoCode;
+var promoPrice;
+var fadeTime = 300;
+const QST = 0.09975;
+const GST = 0.05;
+const fees=6.99;
 
-// Open Cart
-cartIcon.onclick = () => {
-   cart.classList.add("active");
-};
+/* Assign actions */
+$('.quantity input').change(function() {
+  updateQuantity(this);
+});
 
-// Close Cart
-closeCart.onclick = () => {
-   cart.classList.remove("active");
-};
+$('.remove button').click(function() {
+  removeItem(this);
+});
 
+$(document).ready(function() {
+  updateSumItems();
+});
 
-//  Working Cart
-if (document.readyState == 'loading'){
-   document.addEventListener("DOMContentLoaded", ready);
-}else{
-   ready();
-}
+$('.promo-code-cta').click(function() {
 
-// Creating Function
-function ready(){
+  promoCode = $('#promo-code').val();
 
-   // Remove Items From Cart
-   var removeCartButtons = document.getElementsByClassName('cart-remove')
-   console.log(removeCartButtons);
-   for (var i = 0; i < removeCartButtons.length; i++){
-      var button = removeCartButtons[i]
-      button.addEventListener('click', removeCartItem)
-   }
-   // Change Quantity
-   var quantityInputs = document.getElementsByClassName('cart-quantity')
-   for(var i = 0; i < quantityInputs.length; i++){
-      var input = quantityInputs[i];
-      input.addEventListener("change", quantityChanged);
-   }
-   // Add Items To Cart
-   var addCart = document.getElementsByClassName("add-cart");
-   for (var i = 0; i < addCart.length; i++){
-      var button = addCart[i];
-      button.addEventListener("click", addCartClicked);
-   }
-   // Purchase Button
-   document
-      .getElementsByClassName("btn-buy")[0]
-      .addEventListener("click", buyButtonClicked);
-}
+  if (promoCode == '10off' || promoCode == '10OFF') {
+    //If promoPrice has no value, set it as 10 for the 10OFF promocode
+    if (!promoPrice) {
+      promoPrice = 10;
+    } else if (promoCode) {
+      promoPrice = promoPrice * 1;
+    }
+  } else if (promoCode != '') {
+    alert("Invalid Promo Code");
+    promoPrice = 0;
+  }
+  //If there is a promoPrice that has been set show promo
+  if (promoPrice) {
+    $('.summary-promo').removeClass('hide');
+    $('.promo-value').text(promoPrice.toFixed(2));
+    calculateCartTotal(true);
+  }
+});
 
-// Purchase Button
-function buyButtonClicked() {
-   alert('Your order is placed')
-   var cartContent = document.getElementsByClassName("cart-content")[0];
-   while (cartContent.hasChildNodes()){
-      cartContent.removeChild(cartContent.firstChild);
-   }
-   updateTotal();
-}
+/* Recalculate cart */
+function calculateCartTotal(onlyTotal) {
+  var subtotal = 0;
 
+  /* Sum up each row totals */
+  $('.basket-product').each(function() {
+    subtotal += parseFloat($(this).children('.subtotal').text());
+  });
 
-// Remove Items From Cart
-function removeCartItem(event){
-   var buttonClicked = event.target
-   buttonClicked.parentElement.remove( )
-   updateTotal();
-}
+  /* Calculate totals */
+  var total = subtotal + subtotal*QST+subtotal*GST + fees;
 
-// Change Quantity
-function quantityChanged(event){
-   var input = event.target
-   if (isNaN(input.value) || input.value <= 0){
-      input.value = 1;
-   }
-   updateTotal();
-}
+  //If there is a valid promoCode, and subtotal < 10 subtract from total
+  var promoPrice = parseFloat($('.promo-value').text());
+  if (promoPrice) {
+    if (subtotal >= 10) {
+      total -= promoPrice;
+    } else {
+      alert('Order must be more than $10 for Promo code to apply.');
+      $('.summary-promo').addClass('hide');
+    }
+  }
 
-// Add To Cart
-function addCartClicked(event){
-   var button = event.target;
-   var shopProducts = button.parentElement.parentElement.parentElement.parentElement.parentElement;
-   console.log(shopProducts);
-   var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
-   var price = shopProducts.getElementsByClassName("price")[0].innerText;
-   var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
-   addProductToCart(title, price, productImg);
-   updateTotal();
-}
-function addProductToCart(title, price, productImg){
-   var cartShopBox = document.createElement("div");
-   cartShopBox.classList.add("cart-box");
-   var cartItems = document.getElementsByClassName("cart-content")[0];
-   var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
-   for (var i = 0; i < cartItemsNames.length; i++){
-      if (cartItemsNames[i].innerText == title){
-         alert("This item is already added to cart");
-         return;
+  /*If switch for update only total, update only total display*/
+  if (onlyTotal) {
+    /* Update total display */
+    $('.total-value').fadeOut(fadeTime, function() {
+      $('#basket-total').html(total.toFixed(2));
+      $('.total-value').fadeIn(fadeTime);
+    });
+  } else {
+    /* Update summary display. */
+    $('.final-value').fadeOut(fadeTime, function() {
+      $('#basket-subtotal').html(subtotal.toFixed(2));
+      $('#basket-total').html(total.toFixed(2));
+      if (total == 0) {
+        $('.checkout-cta').fadeOut(fadeTime);
+      } else {
+        $('.checkout-cta').fadeIn(fadeTime);
       }
-   }
-
-var cartBoxContent =  `
-      <img src="${productImg}" alt="" class="cart-img">
-      <div class="detail-box">
-         <div class="cart-product-title">${title}</div>
-         <div class="cart-price">${price}</div>
-         <input type="number" value="1" class="cart-quantity">
-      </div>   
-      <!--Remove item from cart-->
-      <i class='bx bxs-trash-alt cart-remove'></i>`;
-
-cartShopBox.innerHTML = cartBoxContent;
-cartItems.append(cartShopBox)
-cartShopBox
-   .getElementsByClassName("cart-remove")[0]
-   .addEventListener("click", removeCartItem);
-cartShopBox
-   .getElementsByClassName("cart-quantity")[0]
-   .addEventListener("change", quantityChanged);
+      $('.final-value').fadeIn(fadeTime);
+    });
+  }
 }
 
-// Update Total
-function updateTotal(){
-   var cartContent = document.getElementsByClassName("cart-content")[0];
-   var cartBoxes = cartContent.getElementsByClassName("cart-box");
-   var total = 0;
-   for (var i = 0; i < cartBoxes.length; i++){
-      var cartBox = cartBoxes[i];
-      var priceElement = cartBox.getElementsByClassName("cart-price")[0];
-      var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
-      var price = parseFloat(priceElement.innerText.replace("$", ""));
-      var quantity = quantityElement.value;
-      total = total + price * quantity;
-   }
-      // Total rounding up
-      total = Math.round(total * 100) / 100;
-      // total = total + (total * 0.15) TAXES
+/* Update quantity */
+function updateQuantity(quantityInput) {
+  /* Calculate line price */
+  var productRow = $(quantityInput).parent().parent();
+  var price = parseFloat(productRow.children('.price').text());
+  var quantity = $(quantityInput).val();
+  var linePrice = price * quantity;
 
+  /* Update line price display and recalc cart totals */
+  productRow.children('.subtotal').each(function() {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.toFixed(2));
+      calculateCartTotal();
+      $(this).fadeIn(fadeTime);
+    });
+  });
 
-      document.getElementsByClassName("total-price")[0].innerText = "$" + total;
-   
+  productRow.find('.item-quantity').text(quantity);
+  updateSumItems();
+}
+
+function updateSumItems() {
+  var sumItems = 0;
+  $('.quantity input').each(function() {
+    sumItems += parseInt($(this).val());
+  });
+  $('.total-items').text(sumItems);
+}
+
+/* Remove item from cart */
+function removeItem(removeButton) {
+  /* Remove row and recalculate the cart total */
+  var productRow = $(removeButton).parent().parent();
+  productRow.slideUp(fadeTime, function() {
+    productRow.remove();
+    calculateCartTotal();
+    updateSumItems();
+  });
 }
