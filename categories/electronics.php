13db8048ea45629ee73ce0fcd6b42e6338287e30
raccoonrick <rjmungcal@gmail.com>
2022-03-15 22:52:59 -0400

    Updated category files
    Adjusted to make use of the new css files

--- categories/electronics.php
+++ categories/electronics.php
@@ -6,7 +6,7 @@ $sql = "SELECT * FROM products WHERE product_category='Electronics'";
 ?>
 <link rel =  "stylesheet" href = "../css/subcat.css">
     <link rel =  "stylesheet" href = "../css/headernfooter.css">
-    <link rel="stylesheet" href="/css/cartStyle.css">
+    <!-- <link rel="stylesheet" href="/css/cartStyle.css"> -->
      <!--ICONS-->
    <script src="https://unpkg.com/boxicons@2.1.1/dist/boxicons.js"></script>
    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
