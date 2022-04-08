d2f14ceb16384029bc5943e355f195fb7d0ddc26
FidaInthesar <78989074+FidaInthesar@users.noreply.github.com>
2022-04-08 17:24:24 -0400

    Update verifyloginTest.php

--- tests/verifyloginTest.php
+++ tests/verifyloginTest.php
@@ -17,36 +17,6 @@ public function testLoginInput() {
             $this->assertTrue(loginValidInput($email,$password));
          
         }
-//    public function testLoginWrongPass(): void
-//     {
-       
-//         //Wrong Password
-       
-//         $this->assertFalse(loginValidInput(__TESTUSERNAME__, 'admine'));
-//     }
 
-//    public function testLoginWrongUsername(): void
-//     {
-       
-//         $this->assertFalse(loginValidInput('admine', __TESTPASSWORD__));
-//     }
-
-//    public function testLoginSuccess(): void
-//     {
-      
-//         //Login Success
-      
-//         $this->assertTrue(loginValidInput(__TESTUSERNAME__, __TESTPASSWORD__));
-//     }
-   
-
-//    function testLogOut(): void
-//    {
-//        //session_start();
-//        $_SESSION = array();
-//        //session_destroy();
-//        //Log Out
-//        $this->assetFalse(fetch_user() > 0);
-//    }
 }
 ?>
